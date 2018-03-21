4ae76caba41e87e970500eb048a1ea53cdd91dbc
ADiligentMan <1003638818@qq.com>
2018-03-21 19:29:09 +0800

    增加了活动收集齐，用于管理活动。

--- app/src/main/java/com/example/acer/myapplication/activity/BaseActivity.java
+++ app/src/main/java/com/example/acer/myapplication/activity/BaseActivity.java
@@ -1,8 +1,28 @@
 package com.example.acer.myapplication.activity;
 
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.acer.myapplication.utility.ActivityCollector;
+
 /**
  * Created by acer on 2018/3/21.
  */
 
-class BaseActivity {
+public class BaseActivity extends AppCompatActivity {
+    public static final  String TAG = "BaseActivity";
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, getClass().getSimpleName());
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
 }
--- app/src/main/java/com/example/acer/myapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/acer/myapplication/activity/MainActivity.java
@@ -5,12 +5,10 @@ import android.os.Bundle;
 
 import com.example.acer.myapplication.R;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a;
     }
 }
