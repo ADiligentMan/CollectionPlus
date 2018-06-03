52db9458be52c7ddd0653d91e93539c8cf9d4dd4
lijiao <lij110397@163.com>
2018-06-03 21:10:07 +0800

    MVVM架构修改后的登录、注册界面 （验证码倒计时按钮任有部分问题）

--- app/src/main/java/com/example/acer/collectionplus/View/MainActivity.java
+++ app/src/main/java/com/example/acer/collectionplus/View/MainActivity.java
@@ -6,8 +6,6 @@ import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.preference.PreferenceManager;
-import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.RadioGroup;
