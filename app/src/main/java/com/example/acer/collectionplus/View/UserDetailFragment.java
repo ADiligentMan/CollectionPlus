cf98e7c3656c524c8e8d8ecb2926171ddeda69e8
李昂 <fanying5227@hotmail.com>
2018-06-07 18:48:20 +0800

    备份

--- app/src/main/java/com/example/acer/collectionplus/View/UserDetailFragment.java
+++ app/src/main/java/com/example/acer/collectionplus/View/UserDetailFragment.java
@@ -1,7 +1,9 @@
 package com.example.acer.collectionplus.View;
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,9 @@ public class UserDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_user_info, container, false);
+
+        Toolbar toolbar = rootView.findViewById(R.id.toolbarId);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
         return rootView;
     }
 }
