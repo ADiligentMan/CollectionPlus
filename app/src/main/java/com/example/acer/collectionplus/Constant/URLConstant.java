b01d07cac60c0eb55c36bb4c251fef7d3bfd1cf0
lijiao <lij110397@163.com>
2018-06-08 15:58:53 +0800

    Merge branch 'mvvm_dev' of https://github.com/ADiligentMan/CollectionPlus into mvvm_dev
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/acer/collectionplus/Constant/URLConstant.java

--- app/src/main/java/com/example/acer/collectionplus/Constant/URLConstant.java
+++ app/src/main/java/com/example/acer/collectionplus/Constant/URLConstant.java
@@ -11,5 +11,9 @@ public class URLConstant {
     public static final String URL_MODIFYUSER="user/modifyInfo.do";
     public static final String URL_MODIFYPASSWORD="user/modifyPassword.do";
     public static final String URL_CHECKENSURECODE="user/checkEnsureCode.do";
-    //public static final String URL_SENDEMAILF="user/sendemailf.do";
+    public static final String URL_GET_RECOM_USER="user/recom_user.do";
+    public static final String URL_GET_RECOM_LINK = "user/recom_link.do";
+    public static final String URL_GET_USER_PUBLIC_COLLECT = "user/public_collect.do";
+    public static final String URL_GET_USER_DETAIL_INFO = "user/detail_info.do";
 }
+
