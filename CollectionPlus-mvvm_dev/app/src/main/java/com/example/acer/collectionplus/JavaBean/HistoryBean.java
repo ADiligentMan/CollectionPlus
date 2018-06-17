4e1cee9fab7421754f5f0c9fc5bcc269a50b2ef1
Lmq <1298636829@qq.com>
2018-06-17 20:36:24 +0800

    浏览历史

---
+++ CollectionPlus-mvvm_dev/app/src/main/java/com/example/acer/collectionplus/JavaBean/HistoryBean.java
@@ -0,0 +1,36 @@
+package com.example.acer.collectionplus.JavaBean;
+
+/**
+ * Created by asus on 2018/6/12.
+ * 浏览历史实体类
+ */
+
+public class HistoryBean {
+  public int id;
+  public String value;
+  public String time;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+  public String getTime() {
+    return time;
+  }
+
+  public void setTime(String time) {
+    this.time = time;
+  }
+}
